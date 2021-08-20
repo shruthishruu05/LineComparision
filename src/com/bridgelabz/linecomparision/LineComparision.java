fdd7ec2a7e1ba12c4c9b3003d936df8d79db6e40
shruthishruu05 <shruthi.m@perfios.com>
2021-08-21 00:38:02 +0530

    ADDED :Welocme message

---
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.*;
+
+public class LineComparision {
+	public static void main(String [] args)
+	{
+	
+		System.out.println("Welcome to line comparision computation program");
+		
+	}
+}
+	
+
+
