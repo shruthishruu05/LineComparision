060ce2ecb5fa7ce5a9bb9eb354a6bb17491fc602
shruthishruu05 <shruthi.m@perfios.com>
2021-08-30 14:27:30 +0530

    ADDED : line class to implement oops concept

--- src/com/bridgelabz/linecomparision/Line.java
+++ src/com/bridgelabz/linecomparision/Line.java
@@ -1,22 +1,22 @@
-package com.bridgelabz.LineComparision2;
+package com.bridgelabz.linecomparision;
 public class Line {
 
 	Point point1 = new Point();
 	Point point2 = new Point();
 
-	public void setPoint1(int x1, int y1) {
+	public void setStartCoordinate(int x1, int y1) {
 		
 		point1.setxCoordinate(x1);
 		point2.setyCoordinate(y1);
 	}
-	public void setPoint2(int x2, int y2 ) {
+	public void setEndCoordinate(int x2, int y2 ) {
 		point1.setxCoordinate(x2);
 		point2.setyCoordinate(y2);
 	}
-	public Point getPoint1() {
+	public Point getStartCoordinate() {
 		return point1;
 	}
-	public Point getPoint2() {
+	public Point getEndCoordinate() {
 		return point2;
 	}
 }
